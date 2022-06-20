6bde24d1c5afd64d2208a7514b35cfae81a98688
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2022-06-20 12:32:09 -0300

    correcciones

---
+++ app.js
@@ -0,0 +1,68 @@
+// let boton = document.getElementById("enviar")
+// boton.onclick = () => {
+//     boton.addEventListener("click", () => {
+//         let nombre = document.querySelector("#nombre").value;
+//         let email = document.querySelector("#email").value;
+//         let tel = document.querySelector("#tel").value;
+//         let mensaje = document.querySelector("#mensaje").value;
+//         console.log();
+//     });
+// }
+
+// let boton = document.getElementById("enviar")
+// boton.onclick = () => {
+//     boton.addEventListener("click", () => {
+//          function capturar (){
+//         let nombre = document.getElementById("nombre").value;
+//         let email = document.getElementById("email").value;
+//         let tel = document.getElementById("tel").value;
+//         let mensaje = document.getElementById("mensaje").value;
+//         console.log(nombre + " " + email + " " + tel + " " + mensaje);
+//     });
+// }
+
+// function respuesta() {
+//     enviar.addEventListener("click", () => {
+//         let nombre = document.querySelector("#nombre").value;
+//         let email = document.querySelector("#email").value;
+//         let tel = document.querySelector("#tel").value;
+//         let mensaje = document.querySelector("#mensaje").value;
+//         console.log(respuesta);
+//     });
+// }
+
+
+
+// <input type="text" name="nombre" id="nombre" placeholder="ingrese el nombre"> <br>
+// <input type="text" name="edad" id="edad" placeholder="ingrese la edad"> <br>
+// <button id="btnPrincipal"> CLICK</button>
+
+//     let boton = document.getElementById("btnPrincipal")
+//     boton.addEventListener("click", captura)
+//     function captura() {
+//         let nombre = document.getElementById("nombre").value;
+//         let edad = document.getElementById("edad").value;
+//         console.log(nombre + " " + edad)
+//     }
+
+
+// let boton = document.getElementById("btnPrincipal")
+// boton.addEventListener("click", captura)
+// function captura() { 
+//     let nombre = document.getElementById("nombre").value;
+//     let email = document.getElementById("email").value;
+//     let tel = document.getElementById("tel").value;
+//     let mensaje = document.getElementById("mensaje").value;
+//     console.log(nombre + " " + email + " " + tel + " " + mensaje);
+// }
+
+
+  let boton = document.getElementById("btnPrincipal")
+  boton.addEventListener("click", captura)
+  function captura() {
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let tel = document.getElementById("tel").value;
+    let mensaje = document.getElementById("mensaje").value;
+    console.log(nombre + " " + email + " " + tel + " " + mensaje)
+  }
